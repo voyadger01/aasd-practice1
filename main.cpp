906c835d28d9d666eb633356a60af270adbf88fb
voyadger01 <voyadgerkx@gmail.com>
2026-02-28 15:43:27 +0300

    done

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ T* merge(const T* a, size_t sa, const T* b, size_t sb, T* c) {
   size_t j = 0;
   size_t k = 0;
   while (i < sa && j < sb){
-    c[k++] = (a[i] < b[j]) ? a[i++] : b[j++];    
+    c[k++] = (a[i] < b[j]) ? a[i++] : b[j++]; // T::operator<
   }
   while (i < sa) {
     c[k++] = a[i++];
@@ -16,3 +16,6 @@ T* merge(const T* a, size_t sa, const T* b, size_t sb, T* c) {
   }
   return c;
 }
+
+// Неявный интерфейс: T::operator<
+// Недостатки явного интерфейса: пользователю самому придётся выделять память под функцию
