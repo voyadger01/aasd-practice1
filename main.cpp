bfe82e9cd1eec8500aaefc42740879e676cad5d0
voyadger01 <voyadgerkx@gmail.com>
2026-02-28 15:37:20 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-int main(){}
+#include <cstddef>
+
+template< class T >
+T* merge(const T* a, size_t sa, const T* b, size_t sb, T* c) {
+  size_t i = 0;
+  size_t j = 0;
+  size_t k = 0;
+  while (i < sa && j < sb){
+    c[k++] = (a[i] < b[j]) ? a[i++] : b[j++];    
+  }
+  while (i < sa) {
+    c[k++] = a[i++];
+  }
+  while (j < sb) {
+    c[k++] = b[j++];
+  }
+  return c;
+}
